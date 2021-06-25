c9b3f3ce981e68a950c314ddd0858463357d45ed
Andrivaio <andrivaio@gmail.com>
2021-06-25 17:05:31 -0300

    upd deu tudo certo
    amem

--- RunnerBoy Project/Assets/Scripts/GameController.cs
+++ RunnerBoy Project/Assets/Scripts/GameController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
     // Propriedade do Chão
     [Header("Configuração do Chão")]
@@ -11,7 +12,7 @@ public class GameController : MonoBehaviour {
     public float _ChaoVelocidade;
     public GameObject _ChaoPrefab;
 
-    // Propriedade do Obstáculo
+    /*// Propriedade do Obstáculo
     [Header("Configuração do Cone")]
     public float _ConeTempo;
     public float _ConeVelocidade;
@@ -27,45 +28,63 @@ public class GameController : MonoBehaviour {
     [Header("Configuração do Prego")]
     public float _PregoTempo;
     public float _PregoVelocidade;
-    public GameObject _PregoPrefab;
+    public GameObject _PregoPrefab; */
+    [Header("Configuração do Bloco de Obstáculos")]
+    public GameObject[] _BlocoObstaculos;
+    public int _BlocoNumero;
+    public float _BlocoTempo;
+    public float _BlocoVelocidade;
 
     [Header("Timer")]
     public float timer;
 
-    void Start() {
-        StartCoroutine("SpawnCone");
+    void Start()
+    {
+        StartCoroutine("SpawnBloco");
+        /*StartCoroutine("SpawnCone");
         StartCoroutine("SpawnPlacaVerde");
-        StartCoroutine("SpawnPrego");
+        StartCoroutine("SpawnPrego");*/
     }
-    void Update() {
+
+    void Update()
+    {
         timer += Time.deltaTime;
-       
+        _BlocoNumero = Random.Range(0, 8);
+
         //PARA TUDO
-        if (PlayerControllerUdemy.vivo == false) {
+        if (PlayerControllerUdemy.vivo == false)
+        {
             _ChaoVelocidade = 0;
-            _ConeVelocidade = 0;
+            /*_ConeVelocidade = 0;
             _PlacaVerdeVelocidade = 0;
             _PregoVelocidade = 0;
 
             StopCoroutine("SpawnCone");
             StopCoroutine("SpawnPlacaVerde");
-            StopCoroutine("SpawnPrego");
+            StopCoroutine("SpawnPrego");*/
+            _BlocoVelocidade = 0;
+            StopCoroutine("SpawnBloco");
+
         }
         //AUMENTO DE DIFICULDADE
-        if (timer >= 60){
-            _ChaoVelocidade = _ChaoVelocidade * 1.2f;
-            _ConeVelocidade = _ConeVelocidade * 1.2f;
-            _PlacaVerdeVelocidade = _PlacaVerdeVelocidade * 1.2f;
-            _PregoVelocidade = _PregoVelocidade * 1.2f;
+        if (timer >= 10)
+        {
+            _ChaoVelocidade = _ChaoVelocidade * 1.1f;
+            /*_ConeVelocidade = _ConeVelocidade * 1.1f;
+            _PlacaVerdeVelocidade = _PlacaVerdeVelocidade * 1.1f;
+            _PregoVelocidade = _PregoVelocidade * 1.1f;
 
             _ConeTempo = _ConeTempo * 0.8f;
             _PlacaVerdeTempo = _PlacaVerdeTempo * 0.5f;
-            _PregoTempo = _PregoTempo * 0.2f;
+            _PregoTempo = _PregoTempo * 0.2f;*/
+
+            _BlocoVelocidade = _BlocoVelocidade * 1.1f;
+            _BlocoTempo = _BlocoTempo * 0.9f; 
 
             timer = 0;
-        }    
+        }
     }
-    IEnumerator SpawnCone() {
+    /*IEnumerator SpawnCone() {
         yield return new WaitForSeconds(_ConeTempo);
         GameObject ObjetoCone = Instantiate(_ConePrefab);
         StartCoroutine("SpawnCone");
@@ -79,5 +98,11 @@ public class GameController : MonoBehaviour {
         yield return new WaitForSeconds(_PregoTempo);
         GameObject ObjetoCone = Instantiate(_PregoPrefab);
         StartCoroutine("SpawnPrego");
+    }*/
+    IEnumerator SpawnBloco()
+    {
+        yield return new WaitForSeconds(_BlocoTempo);
+        GameObject Bloco = Instantiate(_BlocoObstaculos[_BlocoNumero]);
+        StartCoroutine("SpawnBloco");
     }
 }
