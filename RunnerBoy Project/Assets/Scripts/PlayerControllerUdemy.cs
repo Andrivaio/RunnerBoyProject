2dfdac19ca0858bb76dcf674ecfbb9550c5f03c7
Andrivaio <andrivaio@gmail.com>
2021-07-01 00:57:22 -0300

    upd

--- RunnerBoy Project/Assets/Scripts/PlayerControllerUdemy.cs
+++ RunnerBoy Project/Assets/Scripts/PlayerControllerUdemy.cs
@@ -7,7 +7,6 @@ public class PlayerControllerUdemy : MonoBehaviour {
     public bool liberaPulo = false;
     public float forca = 7f;
     public Rigidbody2D playerRB;
-
     public Transform check;
     public LayerMask isGrounded;
     public float raio = 0.2f;
@@ -19,21 +18,28 @@ public class PlayerControllerUdemy : MonoBehaviour {
     public AudioSource track;
     public AudioSource steps;
     public AudioSource jump;
-
+    //ui
     public GameObject GameOverScreen;
     public GameObject Metros;
+    //colliders
+    public GameObject ColliderCorrendo;
+    public GameObject ColliderPulando;
 
     void Start() {
         anim = GetComponent<Animator>();
         steps.Play();
+
     }
-    void Update() {
-        if (vivo == true) {
+    void Update()
+    {
+
+        if (vivo == true)
+        {
             //É CHÃO?
             liberaPulo = Physics2D.OverlapCircle(check.position, raio, isGrounded);
-
             //PULO PC
-            if (Input.GetKeyDown(KeyCode.Space) && liberaPulo == true) {
+            if (Input.GetKeyDown(KeyCode.Space) && liberaPulo == true)
+            {
                 playerRB.AddForce(new Vector2(0, forca), ForceMode2D.Impulse);
                 anim.SetBool("Run", false);
                 anim.SetBool("Jump", true);
@@ -41,7 +47,7 @@ public class PlayerControllerUdemy : MonoBehaviour {
                 jump.Play();
             }
             //PULO TOUCH
-           else if (Input.GetMouseButtonDown(0) && liberaPulo == true)
+            else if (Input.GetMouseButtonDown(0) && liberaPulo == true)
             {
                 playerRB.AddForce(new Vector2(0, forca), ForceMode2D.Impulse);
                 anim.SetBool("Run", false);
@@ -51,7 +57,16 @@ public class PlayerControllerUdemy : MonoBehaviour {
             }
         }
         else steps.Stop();
-    }
+        //COLLIDERS
+        if (liberaPulo == true){
+            ColliderCorrendo.SetActive(true);
+            ColliderPulando.SetActive(false);
+        }
+        if (liberaPulo == false){
+            ColliderCorrendo.SetActive(false);
+            ColliderPulando.SetActive(true);
+        }
+    } 
     //PULO
     private void OnCollisionEnter2D(Collision2D outro) {
         if (outro.gameObject.CompareTag("Ground")) {
